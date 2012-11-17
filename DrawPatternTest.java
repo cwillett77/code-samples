1b14892a6733e6d1555d16cf028250e34f0c670d
Chris Willett <chriswillett77@gmail.com>
2012-11-16 20:00:55 -0800

    initial commit of Draw files

---
+++ DrawPatternTest.java
@@ -0,0 +1,25 @@
+// DrawPatternTest.java
+// Application to display a DrawPattern.
+import javax.swing.JFrame;
+
+public class DrawPatternTest {
+
+	public static void main(String[] args) 
+	{
+		// create a panel that contains our drawing
+		DrawPattern panel = new DrawPattern();
+		
+		// create a new frame to hold the panel
+		JFrame application = new JFrame();
+		
+		// set the frame to exit when it's closed
+		application.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		
+		application.add( panel ); // add the panel to the frame
+		application.setSize( 250, 250 ); // set the size of the frame
+		application.setVisible( true ); // make the frame visible
+
+	} //end main
+
+} //end class DrawPanelTest
+
