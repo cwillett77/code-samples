12d9f3a599f295f940f2e5582306329957306161
Chris Willett <chriswillett77@gmail.com>
2012-11-26 21:12:21 -0800

    Added a basic merge sort (php)

---
+++ merge_sort.php
@@ -0,0 +1,37 @@
+<?
+// Recursive Merge Sort Function
+function merge_sort( $arr ) {
+
+    // if array is only one element, no need to sort it, just return it
+    if ( sizeof ( $arr ) == 1 )
+    	return $arr;
+    	
+    // slice off second half of array and stick it in $arr2
+    $arr2 = array_splice( $arr, ( sizeof( $arr ) / 2 ) );
+    
+    // recursively merge, sort and return
+    return merge( merge_sort( $arr ), merge_sort( $arr2 ) );
+    
+}
+
+// Function to merge two arrays 
+function merge( $arr1, $arr2 ) {
+    
+    // initialize  output array
+    $output = array();
+    
+    // loop through the arrays until they are both empty
+    while( !empty( $arr1 ) || !empty( $arr2 ) ) 
+    	
+    	// if one of the arrays is empty, insert the last value into $output
+    	if ( empty( $arr1 ) || empty( $arr2 ) )
+    		$output[] = ( empty( $arr2 ) ) ? array_shift( $arr1 ) : array_shift( $arr2 );
+    		
+    	// both arrays still have elements, so continue evaluating
+    	else 
+    		$output[] = ( $arr1[ 0 ] <= $arr2[ 0 ] ) ? array_shift( $arr1 ) : array_shift( $arr2 );
+    
+    // return the output array	
+    return $output;
+}
+?>
